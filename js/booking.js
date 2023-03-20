d46baac58e838466b42ebb6fd732233ad7625736
jamestanyh <106426085+jamestanyh@users.noreply.github.com>
2023-03-20 15:01:45 +0800

    Added new files and images

---
+++ js/booking.js
@@ -0,0 +1,14 @@
+/* This is for booking */
+
+const app = Vue.createApp({
+    data() {
+        return {
+        }
+    },
+    created:{
+        //Get data from session storage and make a booking request
+    }
+})
+
+
+app.mount('#app')
